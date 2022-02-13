52822e76578906b7083cba30f8e50f3aeb372927
Austin Clifton <austin.clifton23@gmail.com>
2022-02-12 18:43:02 -0800

    refactor drawing of multichannel pins to be much more generic and take use of the ImGui provided generic

--- src/seam/imgui-utils/properties.cpp
+++ src/seam/imgui-utils/properties.cpp
@@ -7,29 +7,6 @@
 using namespace seam::pins;
 
 namespace seam::props {
-
-	bool Draw(
-		std::string_view label,
-		float& v,
-		float min,
-		float max,
-		float speed,
-		ImGuiSliderFlags flags
-	) {
-		return ImGui::DragFloat(label.data(), &v, speed, min, max, "%.2f", flags);
-	}
-
-	bool Draw(
-		std::string_view label,
-		int& v,
-		int min,
-		int max,
-		float speed,
-		ImGuiSliderFlags flags
-	) {
-		return ImGui::DragInt(label.data(), &v, speed, min, max, "%d", flags);
-	}
-
 	bool Draw(std::string_view label, std::string& v) {
 		char buf[256] = { 0 };
 		// TODO not strcpy
@@ -41,6 +18,7 @@ namespace seam::props {
 		}
 		return false;
 	}
+	
 
 	bool DrawShaderPath(std::string_view label, std::string& path) {
 		// refresh button needs a logo
@@ -49,41 +27,42 @@ namespace seam::props {
 		return refresh_requested || name_changed;
 	}
 
-	bool DrawPin(std::string_view name, pins::PinFloatMeta* pin, float* channels, const size_t size, float speed) {
-		bool changed = false;
-		std::string chan_name;
-		for (size_t i = 0; i < size; i++) {
-			// this should be smarter eventually
-			// like, draw 1-4 channels in the same line
-			chan_name = std::string(name) + "[" + std::to_string(i) + "]";
-			changed = Draw(chan_name, channels[i], pin->min, pin->max, speed) || changed;
-		}
-		return changed;
-	}
-
-	bool DrawPin(std::string_view name, pins::PinIntMeta* pin, int* channels, const size_t size, float speed) {
-		bool changed = false;
-		std::string chan_name;
-		for (size_t i = 0; i < size; i++) {
-			// same as above, this could be smarter
-			chan_name = std::string(name) + "[" + std::to_string(i) + "]";
-			changed = Draw(chan_name, channels[i], pin->min, pin->max, speed) || changed;
-		}
-		return changed;
-	}
-
 	bool DrawPinInput(IPinInput* input) {
-
 		size_t num_channels = 0;
 		void* channels = input->GetChannels(num_channels);
 		switch (input->type) {
 		case PinType::INT: {
 			PinIntMeta* meta = dynamic_cast<PinIntMeta*>(input);
-			return DrawPin(input->name, meta, (int*)channels, num_channels);
+			return ImGui::DragScalarN(
+				input->name.c_str(),
+				ImGuiDataType_S32,
+				channels,
+				num_channels,
+				0.01f,
+				&meta->min,
+				&meta->max,
+				"%d",
+				0
+			);
 		}
 		case PinType::FLOAT: {
 			PinFloatMeta* meta = dynamic_cast<PinFloatMeta*>(input);
-			return DrawPin(input->name, meta, (float*)channels, num_channels);
+			return ImGui::DragScalarN(
+				input->name.c_str(),
+				ImGuiDataType_Float,
+				channels,
+				num_channels,
+				0.001f,
+				&meta->min,
+				&meta->max,
+				"%.3f",
+				0
+			);
+		}
+		case PinType::NOTE_EVENT: {
+			// TODO maybe allow note events to be "mocked" here;
+			// there isn't really anything else to draw for these otherwise.
+			return false;
 		}
 		default:
 			// TODO
--- src/seam/imgui-utils/properties.h
+++ src/seam/imgui-utils/properties.h
@@ -7,32 +7,34 @@
 
 namespace seam {
 	namespace props {
-		/// these are for drawing properties which are not pins
-		bool Draw(
-			std::string_view label,
-			float& v,
-			float min = -FLT_MAX,
-			float max = FLT_MAX,
-			float speed = .001f,
+		/// Draw an add button with an input box and label.
+		/// \param label The name of the button
+		/// \param out_val Will be set to the user input value 
+		/// when the button is pressed
+		/// \return true if the button was pressed
+		template <typename T>
+		bool DrawAddButton(
+			std::string_view label, 
+			T& out_val,
+			T min, 
+			T max, 
+			float speed = 0.01f, 
 			ImGuiSliderFlags flags = 0
-		);
-		bool Draw(
-			std::string_view label,
-			int& v,
-			int min = INT_MIN,
-			int max = INT_MAX,
-			float speed = .001f,
-			ImGuiSliderFlags flags = 0
-		);
+		) {
+			// draw the + button
+			bool pressed = ImGui::Button("+");
+			ImGui::SameLine();
+			// draw the label and input box
+			// note we don't care if the value changed; 
+			// the value only matters when the button was pressed
+			Draw(label, out_val, min, max, speed, flags);
+			return pressed;
+		}
 
 		bool Draw(std::string_view label, std::string& v);
 
 		/// draws a reload button in addition to providing a path setter
 		bool DrawShaderPath(std::string_view label, std::string& path);
-
-		bool DrawPin(std::string_view name, pins::PinFloatMeta* pin, float* channels, size_t len_channels, float speed = 0.01f);
-		
-		bool DrawPin(std::string_view name, pins::PinIntMeta* pin, int* channels, size_t len_channels, float speed = 0.01f);
 
 		/// generic for drawing different input pin types
 		bool DrawPinInput(pins::IPinInput* input);
