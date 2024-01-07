e988398aa154a72954a445a896da2046c479a69e
Austin Clifton <austin.clifton23@gmail.com>
2024-01-07 15:02:43 -0800

    add a Toggle Node, which works nicely with Gate for creating somewhat of a state machine

---
+++ src/seam/nodes/toggle.cpp
@@ -0,0 +1,68 @@
+#include "seam/nodes/toggle.h"
+
+using namespace seam::nodes;
+using namespace seam::pins;
+
+Toggle::Toggle() : INode("Toggle") {
+    ReconfigureInputs(2);
+}
+
+PinInput* Toggle::PinInputs(size_t& size) {
+    size = pinInputs.size();
+    return pinInputs.data();
+}
+
+PinOutput* Toggle::PinOutputs(size_t& size) {
+    size = 1;
+    return &pinOutSelection;
+}
+
+void Toggle::Update(UpdateParams* params) {
+    params->push_patterns->PushSingle(pinOutSelection, &currentState);
+}
+
+bool Toggle::GuiDrawPropertiesList(UpdateParams* params) {
+    uint32_t newCount = statesCount;
+    if (ImGui::DragScalar("States Count", ImGuiDataType_U32, &newCount)) {
+        ReconfigureInputs(newCount);
+        return true;
+    }   
+    return false;
+}
+
+std::vector<seam::props::NodeProperty> Toggle::GetProperties() {
+	using namespace seam::props; 
+
+    std::vector<NodeProperty> props;
+    props.push_back(SetupUintProperty(
+        "States Count", [this](size_t& size) {
+        size = 1;
+        return &statesCount;
+    }, [this](uint32_t* newCount, size_t size) {
+        assert(size == 1);
+        ReconfigureInputs(*newCount);
+    }));
+
+    return props;
+}
+
+void Toggle::ReconfigureInputs(uint32_t newCount) {
+    statesCount = std::max((uint32_t)2, newCount);
+
+    // Add new state triggers.
+    for (size_t i = pinInputs.size(); i < statesCount; i++) {
+        pinInputs.push_back(SetupInputFlowPin(this, 
+            [this, i] {
+                currentState = i;
+            }, 
+            "Toggle " + std::to_string(i))
+        );
+    }
+
+    // Remove any state triggers that should no longer exist.
+    if (statesCount < pinInputs.size()) {
+        pinInputs.resize(statesCount);
+    }
+
+    RecacheInputConnections();
+}
---
+++ src/seam/nodes/toggle.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "seam/nodes/iNode.h"
+#include "seam/pins/pin.h"
+
+using namespace seam::pins;
+
+namespace seam::nodes {
+	/// @brief Toggle between two or more states.
+	/// For now output just ranges from [0..statesCount], but ideally should allow user-defined mappings as well.
+	class Toggle : public INode {
+	public:
+		Toggle();
+        
+		PinInput* PinInputs(size_t& size) override;
+
+		PinOutput* PinOutputs(size_t& size) override;
+
+        void Update(UpdateParams* params) override;
+
+		std::vector<props::NodeProperty> GetProperties() override;
+
+		bool GuiDrawPropertiesList(UpdateParams* params) override;
+
+	private:
+		/// @brief To be called whenever statesCount should be updated.
+		void ReconfigureInputs(uint32_t newStatesCount);
+
+		uint32_t statesCount = 2;
+		uint32_t currentState = 0;
+
+		std::vector<PinInput> pinInputs;
+
+		PinOutput pinOutSelection = SetupOutputPin(this, PinType::UINT, "Selection");
+	};
+}
