91b6cf0569a7bf21f3791cf79efe080b751227c3
Austin Clifton <austin.clifton23@gmail.com>
2024-01-07 15:01:22 -0800

    add fast noise integration from Auburn/FastNoiseLite github repo

---
+++ src/seam/nodes/fastNoise.cpp
@@ -0,0 +1,139 @@
+#include "seam/nodes/fastNoise.h"
+#include "seam/shader-utils.h"
+
+using namespace seam::nodes;
+
+FastNoise::FastNoise() : INode("Fast Noise") {
+    // TEMP: always use UPDATES_OVER_TIME, but there should be a way to flag and unflag
+    // nodes for over-time updates instead.
+    flags = (NodeFlags)(flags | NodeFlags::IS_VISUAL | NodeFlags::UPDATES_OVER_TIME);
+    gui_display_fbo = &fbo;
+
+    // Input pins are set up in the constructor since they require a loop.
+
+    std::array<PinInput, 15> nonEnumInputPins = {
+        SetupInputPin(PinType::INT, this, &resolution, 1, "Resolution", 
+            PinInOptions(sizeof(glm::vec2), 2, 0, std::bind(&FastNoise::ResizeFbo, this))),
+        SetupInputPin(PinType::INT, this, &vSplits, 1, "Vertical Splits"),
+        SetupInputPin(PinType::INT, this, &hSplits, 1, "Horizontal Splits"),
+        SetupInputPin(PinType::FLOAT, this, &speed, 1, "Speed"),
+        SetupInputPin(PinType::BOOL, this, &animateOverTime, 1, "Animate Over Time"),
+        SetupInputPin(PinType::BOOL, this, &enableDomainWarp, 1, "Enable Domain Warp"),
+        SetupInputPin(PinType::INT, this, &seed, 1, "Seed"),
+        SetupInputPin(PinType::FLOAT, this, &frequency, 1, "Frequency"),
+        SetupInputPin(PinType::INT, this, &octaves, 1, "Octaves"),
+        SetupInputPin(PinType::FLOAT, this, &lacunarity, 1, "Lacunarity"),
+        SetupInputPin(PinType::FLOAT, this, &gain, 1, "Gain"),
+        SetupInputPin(PinType::FLOAT, this, &weightedStrength, 1, "Weighted Strength"),
+        SetupInputPin(PinType::FLOAT, this, &pingPongStrength, 1, "Ping Pong Strength"),
+        SetupInputPin(PinType::FLOAT, this, &cellularJitterMod, 1, "Cellular Jitter Mod"),
+        SetupInputPin(PinType::FLOAT, this, &domainWarpAmp, 1, "Domain Warp Amp"),
+    };
+
+    // Exactly this many non-enum pins are expected!
+    assert(pinInputs.size() - fnlOptions.size() == nonEnumInputPins.size());
+
+    std::move(nonEnumInputPins.begin(), nonEnumInputPins.end(), pinInputs.begin());
+
+    for (size_t i = nonEnumInputPins.size(), j = 0; i < pinInputs.size(); i++, j++) {
+        pinInputs[i] = SetupInputPin(PinType::INT, this, &fnlOptions[j].currValue, 1, 
+            fnlOptions[j].optionName, PinInOptions("", &fnlOptions[j].meta));
+    }
+}
+
+void FastNoise::Setup(SetupParams* params) {
+    ResizeFbo();
+    ReloadShaders();
+
+}
+
+void FastNoise::Draw(DrawParams* params) {
+    // Set uniforms; ideally this should be done in less GPU calls???
+    // Each pin could have a callback, but that might be more expensive with binding/unbinding the shader.
+    // A cpu-writable SSBO might be more ideal for flushing info over, idk
+    shader.begin();
+
+    // Advance time if time advancement is enabled.
+    internalTime = internalTime + animateOverTime * params->delta_time * speed;
+    shader.setUniform1f("time", internalTime);
+
+    shader.setUniform2i("resolution", resolution.x, resolution.y);
+    shader.setUniform1i("vSplits", vSplits);
+    shader.setUniform1i("hSplits", hSplits);
+
+    shader.setUniform1i("seed", seed);
+    shader.setUniform1f("frequency", frequency);
+    shader.setUniform1i("octaves", octaves);
+    shader.setUniform1f("lacunarity", lacunarity);
+    shader.setUniform1f("gain", gain);
+    shader.setUniform1f("weightedStrength", weightedStrength);
+    shader.setUniform1f("pingPongStrength", pingPongStrength);
+    shader.setUniform1f("cellularJitterMod", cellularJitterMod);
+    shader.setUniform1f("domainWarpAmp", domainWarpAmp);
+    shader.setUniform1i("enableDomainWarp", enableDomainWarp);
+
+    for (size_t i = 0; i < fnlOptions.size(); i++) {
+        shader.setUniform1i(fnlOptions[i].uniformName, fnlOptions[i].currValue);
+    }
+
+    fbo.begin();
+    fbo.draw(0,0);
+    fbo.end();
+
+    shader.end();
+
+    pinOutFbo.DirtyConnections();
+}
+
+bool FastNoise::GuiDrawPropertiesList(UpdateParams* params) {
+    bool changed = false;
+    for (size_t i = 0; i < fnlOptions.size(); i++) {
+        FnlOption& op = fnlOptions[i];
+        if (ImGui::BeginCombo(op.optionName.data(), op.valueNames[op.currValue])) {
+            for (size_t j = 0; j <= (size_t)op.maxValue; j++) {
+                bool selected = j == (size_t)op.currValue;
+                if (ImGui::Selectable(op.valueNames[j], &selected)) {
+                    changed = true;
+                    op.currValue = j;
+                }
+            }
+            ImGui::EndCombo();
+        }
+    }
+
+    if (ImGui::Button("Reload Shader")) {
+        changed = ReloadShaders() || changed;
+    }
+
+    return changed;
+}
+
+PinInput* FastNoise::PinInputs(size_t& size) {
+    size = pinInputs.size();
+    return pinInputs.data();
+}
+
+PinOutput* FastNoise::PinOutputs(size_t& size) {
+    size = 1;
+    return &pinOutFbo;
+}
+
+void FastNoise::ResizeFbo() {
+    fbo.clear();
+    fbo.allocate(resolution.x, resolution.y, GL_RGB);
+
+    fbo.getTexture().setTextureMinMagFilter(GL_NEAREST, GL_NEAREST);
+}
+
+bool FastNoise::ReloadShaders() {
+    const std::string fragName = "fastNoiseLite.frag";
+    return ShaderUtils::LoadShader(shader, "screen-rect.vert", fragName);
+}
+
+void FastNoise::OnPinConnected(PinConnectedArgs args) {
+	// The output FBO doesn't change; only push it on pin connected.
+	if (args.pinOut->id == pinOutFbo.id) {
+		ofFbo* fbos = { &fbo };
+		args.pushPatterns->Push<ofFbo*>(pinOutFbo, &fbos, 1);
+	}
+}
---
+++ src/seam/nodes/fastNoise.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "seam/nodes/iNode.h"
+
+using namespace seam::pins;
+
+namespace {
+    std::array<const char*, 6> fnlNoiseTypeNames = {
+        "OpenSimplex", "OpenSimplex2s", "Cellular", "Perlin", "Value Cubic", "Value" 
+    };
+
+    std::array<const char*, 3> fnlRotationTypeNames = {
+        "None", "Improve XY Planes", "Improve XZ Planes"
+    };
+
+    std::array<const char*, 6> fnlFractalTypeNames = {
+        "None", "FBM", "Ridged", "Ping Pong", "Domain Warp Progressive", "Domain Warp Independent"
+    };
+
+    std::array<const char*, 4> fnlCellularDistanceTypeNames = {
+        "Euclidean", "Euclidean SQ", "Manhattan", "Hybrid"
+    };
+
+    std::array<const char*, 7> fnlCellularReturnTypeNames = {
+        "Cell Value", "Distance", "Distance2", "Distance2Add", "Distance2Sub", "Distance2Mul", "Distance2Div"
+    };
+
+    std::array<const char*, 3> fnlDomainWarpTypeNames = {
+        "Open Simplex2", "Open Simplex2 Reduced", "Basic Grid"
+    };
+}
+
+namespace seam::nodes {
+    /// @brief Seam Node integration for FastNoiseLite, see:
+    /// https://github.com/Auburn/FastNoiseLite/tree/master
+	class FastNoise : public INode {
+	public:
+		FastNoise();
+
+        void Setup(SetupParams* params) override;
+
+		void Draw(DrawParams* params) override;
+
+		bool GuiDrawPropertiesList(UpdateParams* params) override;
+
+		PinInput* PinInputs(size_t& size) override;
+
+		PinOutput* PinOutputs(size_t& size) override;
+
+        void OnPinConnected(PinConnectedArgs args) override;
+
+	private:
+        void ResizeFbo();
+        bool ReloadShaders();
+
+        /// @brief FastNoiseLite options
+        struct FnlOption {
+            FnlOption(const char* _optionName, const char* _uniformName, int _max, const char** _valueNames) {
+                maxValue = _max;
+                uniformName = _uniformName;
+                optionName = _optionName;
+                valueNames = _valueNames;
+                meta.min = 0;
+                meta.max = _max;
+            }
+
+            int currValue = 0;
+            int maxValue;
+            std::string uniformName;
+            std::string_view optionName;
+            // The length of this array is inferred by the maxValue
+            const char** valueNames;
+            PinIntMeta meta;
+        };
+
+        std::array<FnlOption, 6> fnlOptions = {
+            FnlOption("Noise Type", "noiseType", 
+                fnlNoiseTypeNames.size() - 1, fnlNoiseTypeNames.data()),
+            FnlOption("Rotation Type", "rotationType", 
+                fnlRotationTypeNames.size() - 1, fnlRotationTypeNames.data()),
+            FnlOption("Fractal Type", "fractalType", 
+                fnlFractalTypeNames.size() - 1, fnlFractalTypeNames.data()),
+            FnlOption("Cellular Distance Type", "cellularDistanceType", 
+                fnlCellularDistanceTypeNames.size() - 1, fnlCellularDistanceTypeNames.data()),
+            FnlOption("Cellular Return Type", "cellularReturnType", 
+                fnlCellularReturnTypeNames.size() - 1, fnlCellularReturnTypeNames.data()),
+            FnlOption("Domain Warp Type", "domainWarpType", 
+                fnlDomainWarpTypeNames.size() - 1, fnlDomainWarpTypeNames.data()),
+        };
+
+        // Global configuration
+        glm::ivec2 resolution = glm::ivec2(1024, 1024);
+        int vSplits = 1;
+        int hSplits = 1;
+        int seed = 1337;
+        float frequency = 8.0f;
+        /// @brief Use an internal timer so animation can be smoothly paused and unpaused.
+        float internalTime = 0.f;
+        float speed = 1.f;
+        bool animateOverTime = true;
+        bool enableDomainWarp = false;
+
+        // Fractal type configuration
+        int octaves = 3;
+        float lacunarity = 2.0f;
+        float gain = 0.5f;
+
+        // The octave weighting for all non Domain Warp fractal types.
+        float weightedStrength = 0.0f;
+
+        float pingPongStrength = 2.0f;
+
+        // Will cause artifacts if set > 1.f
+        float cellularJitterMod = 1.0f;
+
+        // "The maximum warp distance from original position when using fnlDomainWarp..."
+        float domainWarpAmp = 1.f;
+
+		ofFbo fbo;
+		ofShader shader;
+
+        // Pins are set up in the constructor!            
+        std::array<PinInput, 21> pinInputs;
+
+        PinOutput pinOutFbo = pins::SetupOutputPin(this, pins::PinType::FBO_RGBA, "Output");
+	};
+}
